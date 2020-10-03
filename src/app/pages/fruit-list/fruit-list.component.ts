1dd4a47ef1c4cfb94d8e8b5dbe54d9c4afb32fce
Kevin Maikath <kevinrx25@gmail.com>
2020-10-03 14:35:15 +0100

    Migrated from CSS to SCSS

--- src/app/pages/fruit-list/fruit-list.component.ts
+++ src/app/pages/fruit-list/fruit-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-fruit-list',
   templateUrl: './fruit-list.component.html',
-  styleUrls: ['./fruit-list.component.css']
+  styleUrls: ['./fruit-list.component.scss']
 })
 export class FruitListComponent implements OnInit {
 
